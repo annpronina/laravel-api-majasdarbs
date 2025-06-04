b431f1cc92dd3d4f2d38335a4aaeceebcee58735
Anna Pronina <anna.proninar1ck4@gmail.com>
2025-06-04 16:25:30 +0300

    added user relationship to comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = ['content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
