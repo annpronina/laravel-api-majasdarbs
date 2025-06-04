3099e4816379b8c30eccdbadffb7e5cd5796bffb
Anna Pronina <anna.proninar1ck4@gmail.com>
2025-06-04 16:24:29 +0300

    added user relationship to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,4 +15,9 @@ class Post extends Model
     {
         return $this->belongsTo(PostStatus::class, 'post_status_id');
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
